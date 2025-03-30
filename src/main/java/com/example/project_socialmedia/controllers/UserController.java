9b3326a6385a262a57ec6a093e0eaa0a595fe0fb
Danny <thanhan1100@gmail.com>
2025-03-30 22:00:53 +0700

    Added GUI react client page into the project

--- src/main/java/com/example/project_socialmedia/controllers/UserController.java
+++ src/main/java/com/example/project_socialmedia/controllers/UserController.java
@@ -1,8 +1,21 @@
 package com.example.project_socialmedia.controllers;
 
+import com.example.project_socialmedia.application.Service.PostService;
+import com.example.project_socialmedia.application.Service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/user")
 public class UserController {
+    private final UserService userService;
+    private final PostService postService;
 
+    // TODO: Add User API
+    // Create
+    // Read
+    // Update
+    // Delete
 }
