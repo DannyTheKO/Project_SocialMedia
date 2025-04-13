f73cb7fa9e42211b9afabde85c0bee5bb262145f
Danny <thanhan1100@gmail.com>
2025-04-13 16:39:46 +0700

    change basePackages on MongoConfig

--- src/main/java/com/project/social_media/infrastructure/Config/MongoConfig.java
+++ src/main/java/com/project/social_media/infrastructure/Config/MongoConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.example.project_socialmedia.domain.Repository")
+@EnableMongoRepositories(basePackages = "com.project.social_media.domain.Repository")
 public class MongoConfig {
 }
